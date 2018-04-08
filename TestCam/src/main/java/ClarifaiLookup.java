b0c9f7a1ec82c62a041694d4c2516e0f64c6d67c
Heena Surve <hfsurve@hotmail.com>
2018-04-08 17:38:45 -0400

    Completed JSON parsing to print out concepts, handled no vision

--- TestCam/src/main/java/ClarifaiLookup.java
+++ TestCam/src/main/java/ClarifaiLookup.java
@@ -7,8 +7,9 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import okhttp3.OkHttpClient;
 import java.io.File;
-import clarifai2.api.ClarifaiResponse;
+import java.util.ArrayList;
 
+import clarifai2.api.ClarifaiResponse;
 public class ClarifaiLookup {
 
     public static void lookupImage(byte[] img){
@@ -19,13 +20,30 @@ public class ClarifaiLookup {
         ClarifaiResponse response = client.getDefaultModels().generalModel().predict()
                 .withInputs(ClarifaiInput.forImage(img))
                 .executeSync();
+        //System.out.print(response.rawBody()
+
         Gson gson = new Gson();
         JsonElement element = gson.fromJson (response.rawBody(), JsonElement.class);
         JsonObject jsonObj = element.getAsJsonObject();
         JsonArray outputs = jsonObj.get("outputs").getAsJsonArray();
-        JsonElement sdfsd = outputs.get(0).getAsJsonObject().get("data");
-        System.out.println(sdfsd);
-        //System.out.println(response.rawBody()
-        ;
+        JsonElement data = outputs.get(0).getAsJsonObject().get("data").getAsJsonObject().get("concepts").getAsJsonArray();
+        ArrayList<String> concepts = new ArrayList<>();
+        for(JsonElement concept: data.getAsJsonArray()){
+            //objects
+            if(concept.getAsJsonObject().get("name").toString().replace("\"","").equals("abstract")){
+                System.out.println("I don't see anything");
+                break;
+            }else {
+                if (concepts.size() < 3) {
+                    if(!concept.getAsJsonObject().get("name").toString().replace("\"", "").equals("portrait"))
+                        concepts.add(concept.getAsJsonObject().get("name").toString().replace("\"", ""));
+                } else {
+                    break;
+                }
+            }
+        }
+        if(!concepts.isEmpty()){
+            System.out.println("I am seeing " + concepts.get(0) + " , " + concepts.get(1) + " , " + concepts.get(2) );
+        }
     }
 }
